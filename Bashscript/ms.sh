4de872a5384403908749fd302ab27d5782f26fdf
Chinazor <128828461+Chinazor93@users.noreply.github.com>
2023-10-28 00:12:57 +0100

    Update ms.sh

--- Bashscript/ms.sh
+++ Bashscript/ms.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-vagrant init ubuntu/focal64
+vagrant init ubuntu/jammy64
 
 cat <<EOF > Vagrantfile
 Vagrant.configure("2") do |config|
@@ -8,7 +8,7 @@ Vagrant.configure("2") do |config|
   config.vm.define "master" do |master|
 
     master.vm.hostname = "master"
-    master.vm.box = "ubuntu/focal64"
+    master.vm.box = "ubuntu/jammy64"
     master.vm.network "private_network", ip: "192.168.20.17"
 
     master.vm.provision "shell", inline: <<-SHELL
@@ -20,7 +20,7 @@ Vagrant.configure("2") do |config|
   config.vm.define "slave" do |slave|
 
     slave.vm.hostname = "slave-1"
-    slave.vm.box = "ubuntu/focal64"
+    slave.vm.box = "ubuntu/jammy64"
     slave.vm.network "private_network", ip: "192.168.20.18"
 
     slave.vm.provision "shell", inline: <<-SHELL
